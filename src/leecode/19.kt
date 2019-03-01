5cc02dfc34eb9db2b916b203145f05a7d12c6efd
zhongshan <zhongshan2010@vip.qq.com>
2019-03-01 15:15:33 +0800

    19.删除链表的倒数第N个节点

--- src/leecode/19.kt
+++ src/leecode/19.kt
@@ -1,7 +1,39 @@
 package leecode
 
 /**
- *
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
  * @author  doive
  * on 2019/3/1 14:43
  */
+fun main(args: Array<String>) {
+    fun removeNthFromEnd(head: ListNode?, n: Int): ListNode? {
+        head?:return null
+        if (n<=0){
+            return head
+        }
+        if (head.next==null&&n==1){
+            return null
+        }
+        var first  = head
+        var target = head
+        var index = 0
+        var secondGo = false
+        while (first?.next!=null){
+            first = first.next
+            index++
+            if (index==n){
+                secondGo = true
+            }
+            if (secondGo){
+                target = target?.next
+            }
+        }
+        // 删除target
+        val next = target?.next
+        if (next!=null){
+            target!!.`val` = next.`val`
+            target.next = next.next
+        }
+        return head
+    }
+}
