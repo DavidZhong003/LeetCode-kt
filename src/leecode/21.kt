3d6335c07b8f79fb580fc6b13fe559c3c976ab48
zhongshan <zhongshan2010@vip.qq.com>
2019-03-04 11:33:46 +0800

    21.合并有序链表

---
+++ src/leecode/21.kt
@@ -0,0 +1,15 @@
+package leecode
+
+/**
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * @author  doive
+ * on 2019/3/2 20:08
+ */
+
+fun main(args: Array<String>) {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+        val head = ListNode(0)
+
+        return head.next
+    }
+}
