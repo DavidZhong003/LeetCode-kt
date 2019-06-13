799a675eec7ef737a709b810e4e62f647a61ab18
zhongshan <zhongshan2010@vip.qq.com>
2019-06-13 16:47:54 +0800

    141. 环形链表

--- src/leecode/141.kt
+++ src/leecode/141.kt
@@ -10,21 +10,19 @@ fun main() {
      * 快慢指针
      * 一个走一步,一个走两步
      */
-    fun hasCycle(head:ListNode?):Boolean{
-        if (head?.next==null){
+    fun hasCycle(head: ListNode?): Boolean {
+        if (head?.next == null) {
             return false
         }
         var slow = head
         var fast = head
-        while (true){
-            if (slow==fast){
-                return true
-            }
-            if (fast?.next == null||fast.next?.next==null){
-                return false
-            }
+        while (fast?.next?.next != null) {
             slow = slow?.next
             fast = fast.next?.next
+            if (slow == fast) {
+                return true
+            }
         }
+        return false
     }
 }
